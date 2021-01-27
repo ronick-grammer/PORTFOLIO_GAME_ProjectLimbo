99cca80036b47f467105d0bed7304c98c981732d
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-01-27 21:47:58 +0900

    Add files via upload

---
+++ AI/ObjPulling.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjPulling 
+{
+    public Transform transform_ObjPullingFrom;
+    public Transform transform_ObjPullingTo;
+    public Transform[] transform_childObj;
+
+}
