562a7048ba7dc6eb18051f118be58055ff700b46
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-01-29 17:50:22 +0900

    Add files via upload

---
+++ Scripts/AnimationControl/Checker_AnimState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Checker_AnimState
+{
+    public static bool Check_AnimState(Animator animator , string name_animState)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(name_animState))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
